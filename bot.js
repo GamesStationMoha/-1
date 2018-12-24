d0f4143b65201d28f49b55784a889edc1fe6fb67
GamesStationMoha <45317067+GamesStationMoha@users.noreply.github.com>
2018-12-24 21:38:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -224,40 +224,30 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return;
 
 
 client.on('message', message => {
-  if (message.author.x5bz) return;
-  if (!message.content.startsWith(prefix)) return;
+if (message.author.codes) return;
+if (!message.content.startsWith(prefix)) return;
 
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
+let command = message.content.split(" ")[0];
+command = command.slice(prefix.length);
 
-  let args = message.content.split(" ").slice(1);
+let args = message.content.split(" ").slice(1);
 
-  if (command == prefix + "ban") {
-               if(!message.channel.guild) return message.reply('** This command only for servers**');
-         
-  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
-  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
-  let user = message.mentions.users.first();
-  let reason = message.content.split(" ").slice(2).join(" ");
-  /*let b5bzlog = client.channels.find("name", "5bz-log");
-  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
-  if (message.mentions.users.size < 1) return message.reply("**يجب عليك منشنه العضو المراد تبنيده**");
-  if(!reason) return message.reply ("**يجب عليك كتابه سبب الباند¯**");
-  if (!message.guild.member(user)
-  .bannable) return message.reply("**لقد تم تبنيدك من السيرفر يرجى مواصله الادارة في حال تم تبنيدك بالغلط**");
+if (command == "ban") {
+             if(!message.channel.guild) return message.reply(':x: **ليس لديك الصلاحيات الكافية**');
+       
+if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply(":x:**انت لا تملك الصلاحيات المطلوبه**");
+if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply(":x: **ليس معي الصلاحيات الكافية**");
+let user = message.mentions.users.first();
 
-  message.guild.member(user).ban(7, user);
+if (message.mentions.users.size < 1) return message.reply("- **منشن شخص**");
+if (!message.guild.member(user)
+.bannable) return message.reply(":x:**يجب ان تكون رتبة البوت اعلي من رتبه الشخص المراد تبنيدة**");
 
-  const banembed = new Discord.RichEmbed()
-  .setAuthor(`BANNED!`, user.displayAvatarURL)
-  .setColor("RANDOM")
-  .setTimestamp()
-  .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
-  .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
-  .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
-  message.channel.send({
-    embed : banembed
-  })
+
+message.guild.member(user).ban(7, user);
+
+message.channel.send(`**Done || ${user.tag} Banned . :ballot_box_with_check: **`)
+
 }
 });
 
